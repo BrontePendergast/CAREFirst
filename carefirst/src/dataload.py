419ad60977a8e0d0254f812091a837ddd18fce6e
Bronte Pendergast <bronpend@gmail.com>
2024-01-31 20:58:48 +1100

    add all guidelines

--- carefirst/src/dataload.py
+++ carefirst/src/dataload.py
@@ -1,14 +1,25 @@
 from langchain_community.document_loaders import PyPDFLoader
 import pickle
 
-# load and split document by page
-loader = PyPDFLoader("../data/guidelines/redcross_guidelines.pdf")
-pages = loader.load_and_split()
+def load_and_store_data(dir = '../data/guidelines/', path = 'redcross_guidelines.pdf'):
+    # load and split document by page
+    loader = PyPDFLoader(dir + path)
+    pages = loader.load_and_split()
 
-# store output as pickle
-with open('../data/guidelines/redcross_guidelines.pickle', 'wb') as f:
-    pickle.dump(pages, f)
+    # store output as pickle
+    with open(dir + path[:-4] + '.pickle', 'wb') as f:
+        pickle.dump(pages, f)
+
+    return f"PDF has converted to text and stored here {dir + path[:-4]+ '.pickle'}"
+
+pdfs = ['redcross_guidelines.pdf', 
+        'ifrc_guidelines.pdf', 
+        'who_guidelines.pdf']
+
+# convert all pdfs
+for pdf in pdfs:
+    load_and_store_data(path = pdf)
 
 # code to open again
-#with open('../data/guidelines/redcross_guidelines.pickle', 'rb') as f:
-#    redcross = pickle.load(f)
+with open('../data/guidelines/ifrc_guidelines.pickle', 'rb') as f:
+   redcross = pickle.load(f)
