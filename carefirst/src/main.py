acf1146832f0a973810c05c81dccac0effc443d7
Jessica Stockham <jhsmith222@gmail.com>
2024-02-13 17:14:46 -0800

    pytest kicking pydantic error

--- carefirst/src/main.py
+++ carefirst/src/main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI, HTTPException
 from typing import List, Dict
 from pydantic import BaseModel, Extra, ValidationError, validator
-# from fastapi_cache import FastAPICache
-# from fastapi_cache.backends.redis import RedisBackend
-# from fastapi_cache.decorator import cache
-# from joblib import load
-# #from redis import asyncio as aioredis
 
-from retrieval import retrieval
+# Model
+from src.retrieval_slim import retrieval
 
 app = FastAPI()
 
@@ -18,15 +14,10 @@ class Retrieved(BaseModel):
     page_content: str
 
 @app.post("/predict", response_model=Retrieved)
-async def predict(input: Query):
-    prediction = retrieval(input)
+async def predict(query: Query):
+    prediction = retrieval(query)
     return {"prediction": prediction}
 
 @app.get("/health")
 async def health():
     return {"status": "healthy"}
-
-#print(retrieval('what cut'))
-
-# test = Query(query='what is burn')
-# print(test.text)
