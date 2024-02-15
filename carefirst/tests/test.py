4bd9c9f1c096acbc1c8ffdeac262fcf2d3d9b2b8
Jessica Stockham <jhsmith222@gmail.com>
2024-02-15 13:04:04 -0800

    add test.py

---
+++ carefirst/tests/test.py
@@ -0,0 +1,24 @@
+import pytest
+from fastapi.testclient import TestClient
+
+
+from src.main import app
+from src.retrieval import retrieval
+
+client = TestClient(app)
+
+def test_docs():
+    response = client.get("/docs")
+    assert response.status_code == 200
+
+# Are we able to make a basic prediction?
+# Do I return the type I expect?
+def test_predict_basic2():
+    data = {"query": "cut"}
+    response = client.post(
+        "/predict",
+        json=data,
+    )
+    print(response)
+    assert response.status_code == 200
+    # assert type(response.json()["prediction"]) is str
