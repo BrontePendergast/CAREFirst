a0761d8e1389e406f425cf42d014d429e30d5e80
Charles Glass <charliedglass@gmail.com>
2024-02-27 03:30:35 -0800

    adding initial web app ui code

---
+++ webapp/client/src/components/TopReasons/TopReasons.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import Container from 'react-bootstrap/Container';
+
+import './TopReasons.css'
+import { OverlayTrigger, Popover, Row, Col } from "react-bootstrap";
+
+function TopReasons({title, reason, icon_class, tooltip, placement}) {
+    return (
+    <Container fluid>
+        <h5 class="bolder-header mb-4">{title}</h5>
+        <ul class="fa-ul">
+            {reason.map((r, i) => (
+                <OverlayTrigger placement={placement} overlay={<Popover id="popover-basic">
+                <Popover.Header as="h3">{r}</Popover.Header>
+                <Popover.Body>
+                {tooltip[i]}
+                </Popover.Body>
+                </Popover>}>
+                <li class="reason-div"><i class={icon_class}></i>
+                    {r}
+                </li>
+                </OverlayTrigger>
+                        
+            ))}
+        </ul>
+    </Container>
+    );
+  }
+  
+
+export default TopReasons;
