2f252ddb74ff6d73036455a7024ad15de8a4f5d4
ricardo.marin <ricardo.marin@pragma.com.co>
2024-03-11 10:44:45 -0500

    Working versino

--- lab4/src/main.py
+++ lab4/src/main.py
@@ -1,15 +1,13 @@
-from fastapi import FastAPI, status, HTTPException
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from typing import List, Optional
-from pydantic import BaseModel, field_validator, Field, ConfigDict
-import joblib
+from typing import Optional
+from pydantic import BaseModel, ConfigDict
 
 import os
 from datetime import datetime
 import string
 import random
 import numpy as np
-import datetime
 
 # Mongo
 import pymongo
@@ -18,7 +16,7 @@ from pydantic_mongo import AbstractRepository, ObjectIdField
 
 # LLM
 from src.db_mongo import getURI
-# from src.llm import *
+from src.llm import ChatChain
 
 # Cache
 from fastapi_cache import FastAPICache
@@ -35,18 +33,18 @@ connection_string= getURI()
 client = pymongo.MongoClient(connection_string)
 database = client["carefirstdb"]
 
-
 class RequestQuery(BaseModel):
-    input: str
+    id: Optional[str] = None
+    query: str
 
 class Response(BaseModel):
+    message_id: Optional[str] = None
     conversation_id: str
-    # message_id: Optional[str] = None
     answer: str
     query: str
     source: str
-    #model: str
-    # timestamp: datetime
+    model: str
+    timestamp: str
 
     model_config = ConfigDict(validate_assignment=True)
 
@@ -54,16 +52,38 @@ class Response(BaseModel):
         for field, value in new_data.items():
             setattr(self, field, value)
 
+class Feedback(BaseModel):
+    id: Optional[str] = None
+    feedback: bool
 
 app = FastAPI()
+
+def getMessageID():
+    # Generate random string of length N
+    N = 7
+    message_id = ''.join(random.choices(string.ascii_uppercase +
+                                string.digits, k=N))
+    return message_id
+
+#Code that Charlie needed to run locally
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://localhost:8000"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 @app.post(
     "/conversations/{conversation_id}",
     response_model=Response
 )
 # @cache(expire=60, coder=PickleCoder)
-async def predict_value(input: RequestQuery) -> Response:
-    
+async def predict_value(conversation_id: str, input: RequestQuery) -> Response:
+    input.id = conversation_id
+
+    ai_response = ChatChain(input.input, input.id)
+    validated_response = Response(**ai_response)
     return {"house_value": input.input}
 
 @app.get(
